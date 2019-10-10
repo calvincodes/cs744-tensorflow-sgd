514ed8fdfc895c79bb0149e1c15e5265416876f1
calvincodes <arpitjain1821@gmail.com>
2019-10-10 12:04:15 -0500

    first commit

---
+++ code_template.py
@@ -0,0 +1,113 @@
+from __future__ import print_function
+import tensorflow as tf
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+
+# define the command line flags that can be sent
+tf.app.flags.DEFINE_integer("task_index", 0, "Index of task with in the job.")
+tf.app.flags.DEFINE_string("job_name", "worker", "either worker or ps")
+tf.app.flags.DEFINE_string("deploy_mode", "single", "either single or cluster")
+FLAGS = tf.app.flags.FLAGS
+
+tf.logging.set_verbosity(tf.logging.DEBUG)
+
+clusterSpec_single = tf.train.ClusterSpec({
+    "worker" : [
+        "localhost:2222"
+    ]
+})
+
+clusterSpec_cluster = tf.train.ClusterSpec({
+    "ps" : [
+        "10.10.1.1:2222"
+    ],
+    "worker" : [
+        "10.10.1.1:2223",
+        "10.10.1.2:2222"
+    ]
+})
+
+clusterSpec_cluster2 = tf.train.ClusterSpec({
+    "ps" : [
+        "10.10.1.1:2222"
+    ],
+    "worker" : [
+        "10.10.1.1:2223",
+        "10.10.1.2:2222",
+        "10.10.1.3:2222",
+    ]
+})
+
+clusterSpec = {
+    "single": clusterSpec_single,
+    "cluster": clusterSpec_cluster,
+    "cluster2": clusterSpec_cluster2
+}
+
+clusterinfo = clusterSpec[FLAGS.deploy_mode]
+server = tf.train.Server(clusterinfo, job_name=FLAGS.job_name, task_index=FLAGS.task_index)
+
+def copied_from_mitron():
+    mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+    # Parameters
+    learning_rate = 0.01
+    training_epochs = 25
+    batch_size = 100
+    display_step = 1
+
+    # tf Graph Input
+    x = tf.placeholder(tf.float32, [None, 784])  # mnist data image of shape 28*28=784
+    y = tf.placeholder(tf.float32, [None, 10])  # 0-9 digits recognition => 10 classes
+
+    # Assigns ops to the local worker by default.
+    with tf.device(tf.train.replica_device_setter(cluster=clusterinfo)):
+        # Set model weights
+        W = tf.Variable(tf.zeros([784, 10]))
+        b = tf.Variable(tf.zeros([10]))
+
+    # Construct model
+    pred = tf.nn.softmax(tf.matmul(x, W) + b)  # Softmax
+
+    # Minimize error using cross entropy
+    cost = tf.reduce_mean(-tf.reduce_sum(y * tf.log(pred), reduction_indices=1))
+    # Gradient Descent
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+    # Initialize the variables (i.e. assign their default value)
+    init = tf.global_variables_initializer()
+
+    # Start training
+    with tf.Session() as sess:
+
+        # Run the initializer
+        sess.run(init)
+
+        # Training cycle
+        for epoch in range(training_epochs):
+            avg_cost = 0.
+            total_batch = int(mnist.train.num_examples / batch_size)
+            # Loop over all batches
+            for i in range(total_batch):
+                batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+                # Run optimization op (backprop) and cost op (to get loss value)
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs,
+                                                              y: batch_ys})
+                # Compute average loss
+                avg_cost += c / total_batch
+            # Display logs per epoch step
+            if (epoch + 1) % display_step == 0:
+                print("Epoch:", '%04d' % (epoch + 1), "cost=", "{:.9f}".format(avg_cost))
+
+        print("Optimization Finished!")
+
+        # Test model
+        correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+        # Calculate accuracy
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
+
+if FLAGS.job_name == "ps":
+    server.join()
+elif FLAGS.job_name == "worker":
+    copied_from_mitron()
